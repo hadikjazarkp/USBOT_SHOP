0a3c613d3481c8d248398b3e95aecceaa9665830
hadikjazarkp <128777913+hadikjazarkp@users.noreply.github.com>
2024-02-06 15:48:26 +0530

    Update settings.py

--- Usbot/settings.py
+++ Usbot/settings.py
@@ -100,11 +100,11 @@ WSGI_APPLICATION = 'Usbot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'USBOT', 
+        'NAME': 'bottle', 
         'USER': 'ammu',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'PASSWORD': 'password',
+        'HOST': 'localhost', 
+        'PORT': '',
     }
 }
 
