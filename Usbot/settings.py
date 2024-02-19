1e7b727f6deb519bf01ec8901a89381c105e6585
hadik <hadik987789@gmail.com>
2024-02-19 14:57:20 +0530

    debug change

--- Usbot/settings.py
+++ Usbot/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['13.201.217.139', 'localhost','127.0.0.1','www.usbot.shop']
 # ALLOWED_HOSTS = []
 
+CROSS_ORGIN = ['https://www.usbot.shop']
 
 # Application definition
 
